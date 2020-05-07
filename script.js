8420128b85d1c1a79d82db65211ea32eac197235
Jeff Qiu <zqiu@uchicago.edu>
2020-05-07 18:02:50 -0500

    added background and fancy font

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ for (let line of lines) {
     line.style.position = "absolute";
     line.style.left = String(Math.random()*w)+"px";
     line.style.top = String(Math.random()*h)+"px";
+    line.style.color = Math.random() > 0.5 ? "rgb(255,113,206)" : "rgb(1,205,254)"
 }
 
 let c2  = document.querySelector(".c1").cloneNode(deep=true);
